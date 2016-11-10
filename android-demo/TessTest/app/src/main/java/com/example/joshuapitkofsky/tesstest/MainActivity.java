faf52cd94154de41e04e3a876a92235733047073
JoshPitkofsky <pitkofskyj@gmail.com>
2016-11-09 19:52:07 -0600

    Currently draws buttons over text, need to set image size before passing to tesseract

--- android-demo/TessTest/app/src/main/java/com/example/joshuapitkofsky/tesstest/MainActivity.java
+++ android-demo/TessTest/app/src/main/java/com/example/joshuapitkofsky/tesstest/MainActivity.java
@@ -14,6 +14,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Picture;
+import android.graphics.Rect;
 import android.graphics.drawable.PictureDrawable;
 import android.net.Uri;
 import android.os.Environment;
@@ -25,8 +26,10 @@ import android.util.Log;
 import android.util.TimingLogger;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.googlecode.leptonica.android.Pixa;
 import com.googlecode.tesseract.android.TessBaseAPI;
 
 import android.view.View;
@@ -38,6 +41,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 
@@ -54,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
     Bitmap image; //our image
     private TessBaseAPI mTess; //Tess API reference
     String datapath = ""; //path to folder containing language data file
-
+    ArrayList<Rect> lst = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //init image
-        image = BitmapFactory.decodeResource(getResources(), R.drawable.test_image);
+        image = BitmapFactory.decodeResource(getResources(), R.drawable.test_url);
 
         //image = BitmapFactory.decodeResource(getResources(), R.drawable.url);
 
@@ -166,18 +170,52 @@ public class MainActivity extends AppCompatActivity {
         long startTime = System.nanoTime();
         String OCRresult = mTess.getUTF8Text();
         String coordinatesAsText = mTess.getBoxText(0);
-        stuff = textCoordinatesToClass(coordinatesAsText);
 
-        Bitmap b = Bitmap.createBitmap(500, 500, Bitmap.Config.ARGB_8888);
-        Canvas c = new Canvas(b);
-        Paint myPaint = new Paint();
-        myPaint.setColor(Color.rgb(255, 0, 0));
-        myPaint.setStrokeWidth(3);
+        Pixa p = mTess.getWords();
+        ArrayList<Rect> lst = p.getBoxRects();
+        String[] parts = OCRresult.split("\\s+");
+        for (int i = 0; i < parts.length; ++i) {
+            if (i >= lst.size()) {
+                Log.d("HELLO:", "Boop");
+                break;
+            }
 
-        c.drawRect(stuff[0].x1, stuff[0].y1, stuff[0].x2, stuff[0].y2, myPaint);
+            ImageView iv = (ImageView)findViewById(R.id.imageView);
+            RelativeLayout rl = (RelativeLayout) findViewById(R.id.ImageContainer);
+            Button bt = new Button(this);
+            bt.setX(lst.get(i).left);
+            bt.setY(lst.get(i).height()*5);
+            bt.setText(parts[i]);
+
+
+            rl.addView(bt);
+
+            //iv.setImageResource(R.drawable.rect);
+
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams((lst.get(i).width())*8, lst.get(i).height()*8);
+            params.leftMargin = lst.get(i).left;
+            params.topMargin = lst.get(i).top;
+
+
+            Bitmap tempBitmap = Bitmap.createBitmap(image.getWidth(), image.getHeight(), Bitmap.Config.RGB_565);
+            Canvas tempCanvas = new Canvas(tempBitmap);
+            //Draw the image bitmap into the cavas
+            Paint myPaint = new Paint();
+            myPaint.setColor(Color.rgb(255, 0, 0));
+            myPaint.setStrokeWidth(3);
+            tempCanvas.drawBitmap(image, 0, 0, null);
+            tempCanvas.drawRoundRect(new RectF(lst.get(i).left,lst.get(i).top,lst.get(i).width(),lst.get(i).height()), 2, 2, myPaint);
+            Log.d("HELLO:", String.valueOf(lst.get(i).left) + ", " + String.valueOf(lst.get(i).top) + ", " +  String.valueOf(lst.get(i).width())+ ", " + String.valueOf(lst.get(i).height()));
+            // lst.get(4).left, lst.get(4).top, lst.get(4).width(), lst.get(4).height();
+            // parts[4]
+
+        }
+
+
 
         // todo allow user to take photo - check
         // draw view with border around it tap on view to follow url. Bounding box around text in one color and around URL in another color
+        //
         // real time
 
         long stopTime = System.nanoTime();
@@ -185,9 +223,10 @@ public class MainActivity extends AppCompatActivity {
         Log.d("OCR'ed Text:", OCRresult);
         TextView OCRTextView = (TextView) findViewById(R.id.OCRTextView);
         OCRTextView.setText(OCRresult);
-        processURL(OCRresult);
-    }
+        //processURL(OCRresult);
 
+
+    }
 
     public void processURL(String string) {
         // separate input by spaces ( URLs don't have spaces )
@@ -196,6 +235,7 @@ public class MainActivity extends AppCompatActivity {
         // Attempt to convert each item into an URL.
         for (String item : parts)
             try {
+                Log.d("ITEM",item);
                 URL url = new URL(item);
                 // If possible then replace with anchor...
 
@@ -251,7 +291,7 @@ public class MainActivity extends AppCompatActivity {
             bitmap = android.provider.MediaStore.Images.Media.getBitmap(cr, mImageUri);
             Log.d("height", String.valueOf(bitmap.getHeight()));
             Log.d("width", String.valueOf(bitmap.getWidth()));
-            Bitmap thumb = Bitmap.createScaledBitmap(bitmap, bitmap.getWidth() / 8, bitmap.getHeight() / 8, false);
+            Bitmap thumb = Bitmap.createScaledBitmap(bitmap, bitmap.getWidth() / 5, bitmap.getHeight() / 5, false);
 
             imageView.setImageBitmap(thumb);
             image = thumb;
